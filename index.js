19a425f8d8192fd6873323c08844bb455b39b957
Akram <akra200m@gmail.com>
2023-02-04 21:21:00 -0500

    1-draw-background-and-snake

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 const FPS = 7;
+const tileCount = 20;
+const tileSize = canvas.width / tileCount;
+const objectSize = tileSize - 2;
+let snakeHeadX = 10;
+let snakeHeadY = 10;
+
+function clearScreen() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawSnake() {
+  ctx.fillStyle = "green";
+  ctx.fillRect(
+    snakeHeadX * tileSize,
+    snakeHeadY * tileSize,
+    objectSize,
+    objectSize
+  );
+}
 
 function drawGame() {
+  clearScreen();
+
+  drawSnake();
   setTimeout(drawGame, 1000 / FPS);
 }
 
